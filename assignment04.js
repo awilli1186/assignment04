4541bd0da3dd28a04be039179fd9239aa7d49544
Adam Williams <awilli1186@gmail.com>
2015-07-09 15:37:28 -0500

    completed homework assignment

---
+++ assignment04.js
@@ -0,0 +1,43 @@
+var givenName;
+// Q: What does `givenName` equal right now?
+// A: undefined
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+// A: Tim
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+// A: JD
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// A: Hello, how are you?
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: 40
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: 45
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 32 years old"
+var born = 1983;
+var today = 2015;
+
+ // A: var age = today - born;
+ //  console.log('Tim is ' + age + ' years old');
+
+// Store some information following in variables.
+var yourName = Adam;
+var instructorName = Will;
+
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+
+// A: Adam is taking a class at The Iron Yard, my instructor's name is Will.
